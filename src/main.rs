bd713b8e73e6f0fa694db827d56de8727bd797ad
Aerial <matas234@gmail.com>
2019-10-01 22:36:39 +0200

    Implement handle_connection, Request struct
    Also implemented Display trait for Request

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,43 @@
 extern crate log;
 extern crate simple_logger;
 
-use std::net::TcpListener;
+use std::fmt;
+use std::io::{Error, Read};
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+
+/// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
+struct Request<'a> {
+    method: &'a str,
+    uri: &'a str,
+    http_version: &'a str,
+}
+
+impl<'a> fmt::Display for Request<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}\r\n", self.method, self.uri, self.http_version)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
+    // 512 bytes is enough for a toy HTTP server
+    let mut buffer = [0; 512];
+
+    // writes stream into buffer
+    stream.read(&mut buffer).unwrap();
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let request_line = request.lines().next().unwrap();
+
+    match parse_request_line(&request_line) {
+        Ok(request) => {
+            info!("\n{}", request);
+        }
+        Err(()) => error!("Badly formatted request: {}", &request_line),
+    }
+
+    Ok(())
+}
 
 fn main() {
     simple_logger::init().unwrap();
@@ -15,7 +51,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => info!("New client!"),
+            Ok(stream) => match handle_connection(stream) {
+                Ok(_) => (),
+                Err(e) => error!("Error handling connection: {}", e),
+            },
             Err(e) => error!("Connection failed: {}", e),
         }
     }
