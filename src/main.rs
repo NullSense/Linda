ce2d047b1e8c45fe35b252b9ca3b3e9ab71041f6
Aerial <matas234@gmail.com>
2019-10-02 00:10:15 +0200

    Add index.html request line validation, uri path

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,55 @@ extern crate log;
 extern crate simple_logger;
 
 use std::fmt;
-use std::io::{Error, Read};
+use std::fs;
+use std::io::{Error, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::path::Path;
 
 /// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
 struct Request<'a> {
     method: &'a str,
-    uri: &'a str,
+    uri: &'a Path,
     http_version: &'a str,
 }
 
 impl<'a> fmt::Display for Request<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {} {}\r\n", self.method, self.uri, self.http_version)
+        write!(
+            f,
+            "{} {} {}\r\n",
+            self.method,
+            self.uri.display(),
+            self.http_version
+        )
     }
+}
+
+fn parse_request_line(request: &str) -> Result<Request, ()> {
+    let mut parts = request.split_whitespace();
+
+    let method = parts.next().ok_or(())?;
+    // We only accept GET requests
+    if method != "GET" {
+        return Err(());
+    }
+
+    let uri = parts.next().ok_or(())?;
+    let uri_path = Path::new(uri);
+    if !uri_path.exists() {
+        return Err(());
+    }
+
+    let http_version = parts.next().ok_or(())?;
+    if http_version != "HTTP/1.1" {
+        return Err(());
+    }
+
+    Ok(Request {
+        method,
+        uri: uri_path,
+        http_version,
+    })
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
@@ -33,34 +67,15 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
     match parse_request_line(&request_line) {
         Ok(request) => {
             info!("\n{}", request);
+            let contents = fs::read_to_string("index.html").unwrap();
+            let response = format!("{}{}", "HTTP/1.1 200 OK\r\n\r\n", contents);
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
         }
         Err(()) => error!("Badly formatted request: {}", &request_line),
     }
 
     Ok(())
-}
-
-fn parse_request_line(request: &str) -> Result<Request, ()> {
-    let mut parts = request.split_whitespace();
-
-    let method = parts.next().ok_or(())?;
-    // We only accept GET requests
-    if !method.contains("GET") {
-        return Err(());
-    }
-
-    let uri = parts.next().ok_or(())?;
-    if !Path::new(uri).exists() {
-        return Err(());
-    }
-
-    let http_version = parts.next().ok_or(())?;
-
-    Ok(Request {
-        method,
-        uri,
-        http_version,
-    })
 }
 
 fn main() {
