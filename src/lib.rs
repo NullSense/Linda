17ab4365d1ba9d3fc6c8df6bc30923e57b27b613
Aerial <matas234@gmail.com>
2019-10-07 13:50:51 +0200

    Propagate errors in handle_connection, add RL error
    Now propagates errors with `?` new RequestLineNotFound error gets sent
    back if the Request-Line does not exist.

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,37 @@ use response::*;
 
 use log::{error, info};
 use std::error::Error;
+use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::{error, fmt};
+
+#[derive(Debug)]
+struct RequestLineNotFound;
+
+impl fmt::Display for RequestLineNotFound {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Request-Line not found")
+    }
+}
+
+impl From<io::Error> for RequestLineNotFound {
+    fn from(error: io::Error) -> Self {
+        RequestLineNotFound
+    }
+}
+
+impl error::Error for RequestLineNotFound {}
 
 #[allow(clippy::unused_io_amount)]
 pub fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buffer = vec![0u8; 1024];
 
     // writes stream into buffer
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer)?;
 
     let request = String::from_utf8_lossy(&buffer[..]);
-    let request_line = request.lines().next().expect("Request line doesn't exist");
+    let request_line = request.lines().next().ok_or(RequestLineNotFound)?;
     info!("Request-Line: {}", &request_line);
 
     // Get response from request_line
@@ -33,10 +52,8 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     match response {
         Ok(mut response) => {
             info!("Response: {}", response);
-            stream
-                .write(&response.format_response())
-                .expect("Couldn't write response");
-            stream.flush().expect("Error flushing stream");
+            stream.write(&response.format_response())?;
+            stream.flush()?;
         }
         Err(e) => error!("Response error: {}", e),
     }
