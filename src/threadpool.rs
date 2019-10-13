dc49e8708fde3c304b12b561bf2867434ce39b56
Aerial <matas234@gmail.com>
2019-10-13 17:45:41 +0200

    Add ico support, threadpool, / rewriting
    A request for / would return a 500 server error, now serves index.html
    if exists, 404.html if it doesn't, as usual.

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,4 +1,19 @@
+use log::info;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
+
+trait FnBox {
+    /// Takes ownership of self and moves value out of Box<T>
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
 
 struct Worker {
     id: usize,
@@ -6,9 +21,22 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
         // create JoinHandle<()> instance
-        let thread = thread::spawn(|| {});
+        let thread = thread::spawn(move || loop {
+            // acquire mutex, panic if it's poisoned (thread panicked while holding lock)
+            // if all is good, receive Job from channel
+            let job = receiver
+                .lock()
+                .expect("Poisoned thread.")
+                .recv()
+                .expect("Thread holding send has shut down.");
+            // Mutex makes sure only a single thread can make a request for a job
+
+            info!("Worker id: {} received job; executing.", id);
+
+            job.call_box();
+        });
 
         Worker { id, thread }
     }
@@ -16,6 +44,7 @@ impl Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -23,13 +52,17 @@ impl ThreadPool {
         // can't have 0 threads, panic
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -37,5 +70,9 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
+
+        // unwrap is ok, because threads don't stop executing
+        // as long as the pool exists, so failure can't happen
+        self.sender.send(job).unwrap();
     }
 }
