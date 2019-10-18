c4b3f5ce96da23c94a21bed222ddf8b876483465
Aerial <matas234@gmail.com>
2019-10-18 14:18:20 +0200

    Add handle_connection doc

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,18 @@ use std::error::Error;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-#[allow(clippy::unused_io_amount)]
+/// Handles TcpStream connection
+///
+/// Writes requests coming from the TcpStream to a buffer
+/// Writes out the response for the corresponding request
+///
+/// # Errors
+///
+/// Propagates errors up if:
+/// * Was not able to read the stream into the buffer
+/// * The request was invlid
+/// * Was not able to write out response
+/// * Was not able to flush the stream
 pub fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0u8; 1024];
 
@@ -33,7 +44,7 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     match response {
         Ok(mut response) => {
             info!("Response: {}", response);
-            stream.write(&response.format_response())?;
+            stream.write_all(&response.format_response())?;
             stream.flush()?;
         }
         Err(e) => error!("Response error: {}", e),
